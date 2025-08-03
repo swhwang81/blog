845e1c023391c67345818db25ba7cb32a8f34d82
Soonwook <swhwang81@gmail.com>
2025-08-03 09:21:35 +0900

    second test react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
    let posts = "개발자 맛집";
    //let [글제목,글제목변경] = useState('코트 추천');  //state 는 변수 대신 데이터 저장 공간
    //  [데이터, 데이터 변경 함수]
-   let [글제목,글제목변경] = useState(['코트 추천','개발자 맛집','MCP 서버']); 
+   let [글제목,글제목변경] = useState(['코트 추천','개발자 맛집','MCP 서버','디자이너 맛집']); 
    let [따봉, 따봉변경] =useState(0);
 
    function 함수(){
@@ -32,6 +32,11 @@ function App() {
       <div className='list'>
         <h3> {글제목[2]} <span>👍</span> {따봉}</h3>
         <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> {글제목[3]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉}</h3>
+        <p>5월 10일 발행</p>
         <hr/>
       </div>
     </div>
