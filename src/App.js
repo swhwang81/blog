4b117a7717a6b7363e34127524cebd7154707394
Soonwook <swhwang81@gmail.com>
2025-08-03 09:06:23 +0900

    first react blog test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+/* esLint-disable */
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   let posts = "개발자 맛집";
+   //let [글제목,글제목변경] = useState('코트 추천');  //state 는 변수 대신 데이터 저장 공간
+   //  [데이터, 데이터 변경 함수]
+   let [글제목,글제목변경] = useState(['코트 추천','개발자 맛집','MCP 서버']); 
+   let [따봉, 따봉변경] =useState(0);
+
+   function 함수(){
+    return 100
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="black-nav">
+       <div>개발 Blog</div>
+     </div>
+     
+      <div className='list'>
+        <h3> {글제목[0]} <span onClick={()=>{alert('따봉!')}}>👍</span> 0</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> {글제목[1]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> {글제목[2]} <span>👍</span> {따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
